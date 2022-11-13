60a37bd83129448c621502796c94cd2085ff1482
Steve <stevealv@hotmail.com>
2022-11-13 16:17:22 -0600

    [Add] Sidebar and Topbar

--- baller-admin/src/App.js
+++ baller-admin/src/App.js
@@ -1,9 +1,26 @@
-
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
+import { Routes } from 'react-router-dom';
+import Sidebar from './scenes/global/Sidebar';
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className='content'>
+            <Topbar />
+            <Routes>
+
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
